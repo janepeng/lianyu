af30bb25b35b0221c6d226625c8317764e9f336b
Jane Wang <janewang@Janes-MacBook-Air.local>
2018-04-10 22:47:45 -0700

    first commit

---
+++ process_opponent_company.js
@@ -0,0 +1,26 @@
+function hasGetUserMedia() {
+	return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+function loadVideoDiv() {
+	var videoDiv = '<video autoplay></video>';
+
+	// put div in dom
+	$(videoDiv).insertAfter('.table:first');
+
+	const constraints = { video: true };
+	const video = document.querySelector('video');
+
+	navigator.mediaDevices.getUserMedia(constraints)
+		.then(function(stream) {
+			video.srcObject = stream;
+		}).catch(function(error) {
+			console.error('Rejected', error);
+		});
+}
+
+if (hasGetUserMedia()) {
+	alert('woohoo')
+} else {
+	alert('getUserMedia() is not supported by your browser');
+}
