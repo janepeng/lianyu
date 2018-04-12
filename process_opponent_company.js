8a6b61fd4c901299c86007a7caf625fa76da0fa9
Jane Wang <janewang@Janes-MacBook-Air.local>
2018-04-11 19:36:58 -0700

    test

--- process_opponent_company.js
+++ process_opponent_company.js
@@ -2,16 +2,27 @@ function hasGetUserMedia() {
 	return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
 
+var videoDiv = '<video autoplay></video>';
+var videoDropdown = '<div class="select">
+	<label for="videoSource">Video source: </label><select id="videoSource"></select>
+	</div>';
+var img = '<img src="">';
+var canvas = '<canvas style="display:none;"></canvas>';
+
+// put div in dom
+$(videoDropdown).insertAfter('.table:first');
+$(videoDiv).insertAfter('.table:first');
+
+const constraints = {
+	video: {width: {min: 360}, height: {min: 360}}
+};
+const video = document.querySelector('video');
+var videoSelect = document.querySelector('select#videoSource');
+videoSelect.onchange = getStream;
+
 function loadVideoDiv() {
-	var videoDiv = '<video autoplay></video>';
-
-	// put div in dom
-	$(videoDiv).insertAfter('.table:first');
-
-	const constraints = {
-		video: {width: {min: 1280}, height: {min: 720}}
-	};
-	const video = document.querySelector('video');
+	navigator.mediaDevices.enumerateDevices()
+  		.then(gotDevices).then(getStream);
 
 	navigator.mediaDevices.getUserMedia(constraints)
 		.then(function(stream) {
@@ -21,9 +32,42 @@ function loadVideoDiv() {
 		});
 }
 
+function gotStream(stream) {
+  videoElement.srcObject = stream;
+}
+
+function getStream() {
+  var constraints = {
+    audio: {
+      deviceId: {exact: audioSelect.value}
+    },
+    video: {
+      deviceId: {exact: videoSelect.value}
+    }
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).
+    then(gotStream);
+}
+
+function gotDevices(deviceInfos) {
+	for (var i = 0; i !== deviceInfos.length; ++i) {
+		var deviceInfo = deviceInfos[i];
+		var option = document.createElement('option');
+		option.value = deviceInfo.deviceId;
+		if (deviceInfo.kind === 'videoinput') {
+			option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1);
+			videoSelect.appendChild(option);
+		}
+	}
+}
+
 if (hasGetUserMedia()) {
-	alert('woohoo')
 	loadVideoDiv();
 } else {
 	alert('getUserMedia() is not supported by your browser');
 }
+
+
+
+
